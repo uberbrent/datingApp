455e8d8708c9a60770b7565bb76beff038208107
uberbrent <bmjohns1337@gmail.com>
2023-10-25 20:34:42 -0500

    added ssl cert

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
