080b86ba7f823310110fe036fd327826ee73ea1e
uberbrent <bmjohns1337@gmail.com>
2023-10-26 14:13:56 -0500

    added api endpoint for adding users and converted object into string. Added password hashing and salting

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class BaseApiController : ControllerBase
+    {
+        
+
+    }
+
+}
+
