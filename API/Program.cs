cc05a44ac194e4fe52ed0ef52913211e4f69767d
uberbrent <bmjohns1337@gmail.com>
2023-10-25 20:08:17 -0500

    full front and back end walking skeleton angular is reading back end API

--- API/Program.cs
+++ API/Program.cs
@@ -11,15 +11,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
