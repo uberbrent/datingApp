adf8faed7f47a1218f18a6f660c93c340d5fbb99
uberbrent <bmjohns1337@gmail.com>
2023-11-05 11:42:31 -0600

    added member mapping to retrieve only useful and needed data

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,6 +1,18 @@
-﻿namespace API;
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
 
-public class AutoMapperProfiles
+namespace API.Helpers;
+
+public class AutoMapperProfiles : Profile
 {
-
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl,
+                 opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+        CreateMap<Photo, PhotoDto>();
+    }
 }
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -9,5 +9,7 @@ namespace API.Interfaces
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
     }
 }
