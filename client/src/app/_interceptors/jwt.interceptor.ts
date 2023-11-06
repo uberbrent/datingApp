45b9d680a96e1a672db8f92476b1b75daed6f9ce
uberbrent <bmjohns1337@gmail.com>
2023-11-06 16:48:57 -0600

    added styling and front end functionality for the members area as well as individual profiles. Updated the app to use an interceptor to pass the JWT

--- client/src/app/_interceptors/jwt.interceptor.ts
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -5,14 +5,27 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
+import { AccountService } from '../_services/account.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if(user) {
+          request = request.clone({
+            setHeaders: {
+              Authorization: `Bearer ${user.token}`
+            }
+          })
+        }
+      }
+    })
+
     return next.handle(request);
   }
 }
